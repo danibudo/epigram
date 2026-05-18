49a2e9243f9b0b163bf6ba8f4361b03d8037f223
Dani Budo <56448641+danibudo@users.noreply.github.com>
2026-05-18 19:16:48 +0200

    initial commit API

---
+++ epigram-api/src/test/java/com/dani/epigramapi/EpigramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.epigramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpigramApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
